21404782cbf66ebdf444003a5a099ca456a40fac
Sam Riley <sam@lovecrafts.com>
2022-01-28 21:24:11 +0000

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+import { Config } from '@verdaccio/types';
+
+export interface S3Config extends Config {
+  bucket: string;
+  keyPrefix: string;
+  endpoint?: string;
+  region?: string;
+  s3ForcePathStyle?: boolean;
+  acl?: string;
+  tarballACL?: string;
+  accessKeyId?: string;
+  secretAccessKey?: string;
+  sessionToken?: string;
+}
